b0940a06a5cfbafa959c0101d3e860eaa9f7c5aa
AgustinSegui <agussegui24@hotmail.es>
2024-10-03 22:35:59 -0300

    feat:Change a new description about us, new prices and new descripcion about the destinations

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,9 @@ import{
 const router = express.Router();
 
 router.get('/', paginaInicio);
-
 router.get('/nosotros', paginaNosotros);
-
 router.get('/viajes', paginaViajes);
-
 router.get('/viajes/:slug', paginaDetalleViaje);
-
 router.get('/testimoniales', paginaTestimoniales);
 router.post('/testimoniales', guardarTestimonial);
 
